65aa5b311aef4fe749be47be6ebf2292a8ae949a
itsHamdySalem <hamdysalem112002@gmail.com>
2023-12-16 15:37:15 +0200

    Adding BoothMultiplier;

---
+++ code/BoothMultiplier/BoothMultiplier.v
@@ -0,0 +1,14 @@
+
+module BoothMultiplier (
+    input [31:0] A,
+    input [31:0] B,
+    output [63:0] F
+);
+
+    reg [63:0] product;
+    reg [4:0] counter;
+    reg [1:0] state;
+
+    // TODO: Implement a 32-bit multiplier using 'Booth's Algorithm'
+
+endmodule
