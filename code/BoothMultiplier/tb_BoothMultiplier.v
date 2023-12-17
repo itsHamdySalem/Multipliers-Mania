dd4c84c680c46a8759ca3ab61edfd75a171a295a
itsHamdySalem <hamdysalem112002@gmail.com>
2023-12-17 23:30:28 +0200

    BoothMultiplier: fixing bugs, adding sim files;

--- code/BoothMultiplier/tb_BoothMultiplier.v
+++ code/BoothMultiplier/tb_BoothMultiplier.v
@@ -3,8 +3,8 @@
 module tb_BoothMultiplier;
 
     reg CLK, RESET;
-    reg [31:0] A, B;
-    wire [63:0] F;
+    reg signed [31:0] A, B;
+    wire signed [63:0] F;
 
     BoothMultiplier UUT (
         .CLK(CLK),
@@ -24,7 +24,7 @@ module tb_BoothMultiplier;
         // Test Case 1: Multiplication of positive and negative number
         A = 15;
         B = -31;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#1: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -34,7 +34,7 @@ module tb_BoothMultiplier;
         // Test Case 2: Multiplication of positive and positive number
         A = 13;
         B = 29;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#2: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -44,7 +44,7 @@ module tb_BoothMultiplier;
         // Test Case 3: Multiplication of negative and negative number
         A = -81;
         B = -55;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#3: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -54,7 +54,7 @@ module tb_BoothMultiplier;
         // Test Case 4: Multiplication of negative and positive number
         A = -100;
         B = 6;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#4: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -64,7 +64,7 @@ module tb_BoothMultiplier;
         // Test Case 5: Multiplication by zero
         A = 0;
         B = -300;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#5: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -74,7 +74,7 @@ module tb_BoothMultiplier;
         // Test Case 6: Multiplication by 1
         A = 122;
         B = 1;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#6: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -84,7 +84,7 @@ module tb_BoothMultiplier;
         // Test Case 7: Additional random test case
         A = 1577;
         B = -40;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#7: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
@@ -94,7 +94,7 @@ module tb_BoothMultiplier;
         // Test Case 8: Additional random test case
         A = -12340;
         B = -54321;
-        #10
+        #20
         if (F === A * B) begin
             $display("TestCase#8: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
         end else begin
